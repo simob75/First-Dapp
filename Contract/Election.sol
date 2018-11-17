516d09bb3f63205cfca88367c803dfcffb34724d
Bruna Simonian <simob75@users.noreply.github.com>
2018-11-17 12:20:47 +0000

    Add files via upload

---
+++ Contract/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+contract Election {
+    string public candidateName;
+
+    constructor () public {
+        candidateName = "Candidate 1";
+    }
+
+    function setCandidate (string _name) public {
+        candidateName = _name;
+    }
+}
